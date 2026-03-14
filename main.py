934cb8519d1e3f009a30bbdb3b0504e91413c87f
Jakub <jakub.konecki9@gmail.com>
2026-03-14 15:52:30 +0100

    feat: add feature sliders and heatmap visualization

--- main.py
+++ main.py
@@ -13,8 +13,9 @@ if __name__=="__main__":
         print("Error: ",e)
         sys.exit(1)
 
-    input_info = session.get_inputs()
-    number_of_features = input_info[0].shape[-1] #TODO - do it better, more error proof. Only 1d supported
+    input_info = session.get_inputs()[0]
+    number_of_features = input_info.shape[-1] #TODO - do it better, more error proof. Only 1d supported
+    input_name = input_info.name
     # print(number_of_features)
     # for i in input_info:
     #     print(f"Nazwa wejścia: {i.name}")
@@ -57,15 +58,37 @@ if __name__=="__main__":
             else:
                 state_values[feature] = st.slider(f"Feature {feature}",float(min_v),float(max_v),value=0.0,key=key_sld)
         
-        if st.button("Show graph"):
-            x_range = np.linspace(0,1,SIZE)
-            y_range = np.linspace(0,1,SIZE)
-            xx, yy = np.meshgrid(x_range, y_range)
-            flat_x, flat_y = xx.ravel(), yy.ravel()
+    if st.button("Show graph"):
+        x_range = np.linspace(0,1,SIZE)
+        y_range = np.linspace(0,1,SIZE)
+        xx, yy = np.meshgrid(x_range, y_range)
+        flat_x, flat_y = xx.ravel(), yy.ravel()
 
-            for i in range(number_of_features):
-                if i!=x_axis and i !=y_axis:
-                    
+        input_data = np.zeros(shape=(SIZE*SIZE,number_of_features))
+
+        for i in range(number_of_features):
+            if i==x_axis:
+                input_data[:,i] = flat_x
+            elif i==y_axis:
+                input_data[:,i] = flat_y
+            else:
+                input_data[:,i] = state_values[i]
+
+        input_data = input_data.astype(np.float32)
+        outputs = session.run(None, {input_name: input_data})
+        prediction = outputs[0]
+        if prediction.shape[1] > 1:
+            z_val = np.argmax(prediction,axis=1)
+        else:
+            z_val = prediction.flatten()
+        z_grid = z_val.reshape(SIZE,SIZE)
+
+        fig, ax = plt.subplots()
+        c = ax.contourf(xx, yy, z_grid, cmap='RdYlBu')
+        fig.colorbar(c)
+        st.pyplot(fig)
+
+
 
             
             
